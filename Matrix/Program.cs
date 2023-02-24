a8898460af4660bbe1ffa46f2049058dba98bd8d
VictorWorkComp <v.goriachyi@bankvostok.com.ua>
2023-02-24 21:00:03 +0200

    done

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,23 +1,41 @@
 ﻿using MatrixProject;
+using System.Diagnostics;
+
+var isCorrectInput = false;
+
+while (isCorrectInput == false) 
+{
+    var colCount = MatrixHelper.InputDimension(DimensionType.ColumnsCount);
+    var rowCount = MatrixHelper.InputDimension(DimensionType.RowsCount);
+
+    try
+    {
+        var matrix = MatrixHelper.GetMatrix(rowCount, colCount);
+        isCorrectInput = true;
+        Console.Write("Do u want to fill matrix random? ([y] to confirm)");
+        var input = Console.ReadLine();
+        if (input == "y")
+            matrix.FillRandom();
+        else
+            matrix.FillConsistently();
+
+        MatrixHelper.PrintToConcole(matrix);
+
+        Console.WriteLine("Matrix track: " + matrix.Track());
+
+        Console.WriteLine("Shake print: " + matrix.PrintSnake());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("TRY AGAIN! \n");
+    }
+}
 
 
-var colCount = MatrixHelper.InputDimension(DimensionType.ColumnsCount);
-var rowCount = MatrixHelper.InputDimension(DimensionType.RowsCount);
 
-var matrix = MatrixHelper.GetMatrix(rowCount, colCount);
 
-Console.Write("Do u want to fill matrix random? ([y] to confirm)");
-var input = Console.ReadLine();
-if (input == "y")
-    matrix.FillRandom();
-else
-    matrix.FillConsistently();
 
-MatrixHelper.PrintToConcole(matrix);
 
-Console.WriteLine("Matrix track: " + matrix.Track());
-
-Console.WriteLine("Shake print: " + matrix.PrintSnake());
 
 
 
